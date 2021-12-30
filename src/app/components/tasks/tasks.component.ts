62eddbb0da4853c32ce96a833655e43af224cf8c
Malik Dunston <hello@malikdunston.com>
2021-12-30 18:53:32 -0500

    1:11:51 in yt video

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -14,6 +14,7 @@ export class TasksComponent implements OnInit {
 		this.taskService.getTasks().subscribe((tasks)=>this.tasks = tasks);
 	}
 	deleteTask(task: Task){
-		console.log(task)
+		console.log("task")
+		this.taskService.deleteTask(task).subscribe(()=>this.tasks = this.tasks.filter(t => t.id !== task.id));
 	}
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -7,9 +7,13 @@ import { Task } from '../Task';
 	providedIn: 'root'
 })
 export class TaskService {
-	private url = "http://localhost:5000/tasks"
+	private apiUrl = "http://localhost:5000/tasks"
 	constructor(private http:HttpClient) { }
 	getTasks(): Observable<Task[]> {
-		return this.http.get<Task[]>(this.url);
+		return this.http.get<Task[]>(this.apiUrl);
+	}
+	deleteTask(task: Task): Observable<Task>{
+		const url = `${this.apiUrl}/${task.id}`
+		return this.http.delete<Task>(url)
 	}
 }
