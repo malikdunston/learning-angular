c319264cd2306a434f21ad8c00fa544d417b4932
Malik Dunston <hello@malikdunston.com>
2021-12-30 23:34:15 -0500

    two-way binding

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
 	selector: 'app-add-task',
 	templateUrl: './add-task.component.html',
 	styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
-	time: string;
+	text: string;
 	day: string;
 	reminder: boolean = false;
 	constructor() { }
