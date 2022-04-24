4731406e5a31889283c6b3981fed3b2bc45c773b
Malik Dunston <hello@malikdunston.com>
2022-04-24 19:21:01 -0400

    Routes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-header',
@@ -11,16 +12,19 @@ export class HeaderComponent implements OnInit {
 	title: string = 'Task Tracker App';
 	showAddTask: boolean = false;
 	subscription: Subscription;
-	constructor(private uiService:UiService) { 
+	constructor(private uiService:UiService, private router:Router) { 
 		this.subscription = this.uiService
 			.onToggle()
 			.subscribe((value) => (this.showAddTask = value))
 	}
 	ngOnInit(): void { // like compdidmount or useeffect...
-		// console.log(this);
+		// console.log(this.router);
 		// good place to place http reqs...
 	}
 	toggleAddTask(): void{
 		this.uiService.toggleAddTask();
+	}
+	hasRoute(route: string){
+		return this.router.url === route;
 	}
 }
